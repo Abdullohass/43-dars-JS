20217cb1805e3544d0129b44f870154b873d45ba
Abdullohass <abdulloh.s.gk@gmail.com>
2024-09-07 15:54:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// let ism="Abdulloh";
+// let yosh=25;
+
+let a=prompt("String type da malumot kiriting?")
+let b=+prompt("Number type da malumot kiriting?")
+let c=prompt("Boolean type da malumot kiriting?")
+
+alert(" Siz kiritgan "+a+" sozi "+(typeof a)+ "type ga tegishli\n" +" Siz kiritgan "+b+
+" sozi "+(typeof b)+ "type ga tegishli\n"+" Siz kiritgan "+c+
+" sozi "+(typeof c)+ "type ga tegishli")
+
+
+
+
+
+
+
+
+
